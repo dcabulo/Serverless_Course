720628738e8539075fb390e59629eb9472d3208c
dcabulo <diego.cabulo@linktic.co>
2021-03-07 09:49:04 -0500

    finish 2 examples

--- examples/2-basic-api/app.js
+++ examples/2-basic-api/app.js
@@ -1,1 +1,33 @@
-const result = document.querySelector('.result')
+const result = document.querySelector(".result");
+
+const fetchData = async () => {
+  try {
+    //const { data } = await axios.get("/.netlify/functions/1-hello");
+    const { data } = await axios.get("/api/2-basic-api");
+    const product = data
+      .map((item) => {
+        const {
+          image: { url },
+          name,
+          price,
+        } = item;
+        return `<article class="product">
+      <img
+        src="${url}"
+        alt="${name}"
+      />
+      <div class="info">
+        <h5>${name}</h5>
+        <h5 class="price">$${price}</h5>
+      </div>
+    </article>`;
+      })
+      .join("");
+    result.innerHTML = product;
+  } catch (er) {
+    result.innerHTML = `<h4>There was an error, please try againg later</h4>`;
+    //result.textContent = er.response.data;
+  }
+};
+
+fetchData();
---
+++ functions/2-basic-api.js
@@ -0,0 +1,9 @@
+//domain/.netlify/1-hello
+const items = require("../assets/data");
+
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(items),
+  };
+};
